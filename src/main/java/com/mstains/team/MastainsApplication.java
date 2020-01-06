db185b9cc6676a9a8f17136253b8ec32075626df
浅墨兰 <344330607@edufree.win>
2020-01-06 10:10:37 +0800

    增加项目

---
+++ src/main/java/com/mstains/team/MastainsApplication.java
@@ -0,0 +1,13 @@
+package com.mstains.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastainsApplication {
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MastainsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mstains/team/MastainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mstains.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
